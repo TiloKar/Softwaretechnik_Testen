733354ac58bcffb0bf7266c67feb750e1bece6d1
Tilo <85075339+TiloKar@users.noreply.github.com>
2021-06-13 14:56:05 +0200

    main eingefügt
    eine main für aufgabe mit dem ant file angefügt, um RUN zu testen

--- HonorarRechner.java
+++ HonorarRechner.java
@@ -1,5 +1,12 @@
 
 public class HonorarRechner{
+	
+	
+	public static void main (String[] args) {
+		System.out.println("Ich lebe und habe: " + args[0] + "als Argument bekommen");
+	}
+	
+	
 	TextAnalyze r;
 	public HonorarRechner() {
 		this.r = new TextAnalyze();
@@ -14,20 +21,20 @@ public class HonorarRechner{
 	}
 	/**
 	 * 
-	 * @param text zeichenkette für honorarberechnung
+	 * @param text zeichenkette fÃ¼r honorarberechnung
 	 * @return honorar nach hinterlegter formel
 	 */
 	public int getSalery(String text) {
-		//if (text==null) return 0;  //für Exception bei testfall null ausgeklammert
+		//if (text==null) return 0;  //fÃ¼r Exception bei testfall null ausgeklammert
 		return r.getPictureCount(text) * 50 + text.length();
 	}
-	/**  ruft getSalery auf, Fallbeispiel um this.getSalery später mit spy selektiv zu mocken	
+	/**  ruft getSalery auf, Fallbeispiel um this.getSalery spÃ¤ter mit spy selektiv zu mocken	
 	 * 
-	 * @param text zeichenkette für honorarberechnung
+	 * @param text zeichenkette fÃ¼r honorarberechnung
 	 * @return honorar nach hinterlegter formel + offset
 	 */
 	public int getBetterSalery(String text) {
-		//if (text==null) return 0;  //für Exception bei testfall null ausgeklammert
+		//if (text==null) return 0;  //fÃ¼r Exception bei testfall null ausgeklammert
 		return this.getSalery(text) + 450;
 	}
 
