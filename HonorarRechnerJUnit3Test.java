26719c6621025d3c79c40aef1bf6356c5f2221a8
Tilo <85075339+TiloKar@users.noreply.github.com>
2021-06-13 14:20:38 +0200

    JUnit 3 testklase für Aufgabe mit dem antfile

---
+++ HonorarRechnerJUnit3Test.java
@@ -0,0 +1,22 @@
+
+import static org.junit.Assert.assertEquals;
+
+import junit.framework.TestCase;
+
+public class HonorarRechnerJUnit3Test extends TestCase {
+
+	public void testPictureCount() {
+		String s="ich schreibe, schreibe <picture>, oh seht "
+				+ "euch dieses bild an. und da noch eins <picture>";
+		TextAnalyze t = new TextAnalyze();
+		assertEquals(	2,t.getPictureCount(s));	 						
+	}
+	
+	public void testCalculatorWithDep() {
+		String s="ich schreibe, schreibe <picture>, oh seht "
+				+ "euch dieses bild an. und da noch eins <picture>";
+		TextAnalyze t = new TextAnalyze();
+		HonorarRechner r = new HonorarRechner();
+		assertEquals(	s.length() + 50*t.getPictureCount(s), r.getSalery(s));						
+	}
+}
